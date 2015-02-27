d6bd892c83e4047d94187873cfa1f36e3d46e032
IV007 <ivanmcschool@gmail.com>
2015-02-26 21:20:05 -0800

    complete code

--- CellFire Code/src/Car.java
+++ CellFire Code/src/Car.java
@@ -1,4 +1,22 @@
 
 public class Car {
+	private String model;
+	private int mileage;
+	private int year;
+	private String color;
+
+	
+	public int getMileage(int miles){
+		return miles;
+	}
+	public int drive(int driven){
+
+		int[] anArray = new int[9];
+		return driven;
+	}
+	Car cellFire = new Car();
+	
+	
+
 
 }
